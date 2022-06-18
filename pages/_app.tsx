f70760fd368882b5614e0d81d5cff09e060961a4
Gabriel P??rez <gabriel@garox.org>
2022-06-17 20:30:08 -0400

    add page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react'
+import { Toaster } from 'react-hot-toast'
 import { CssBaseline, GeistProvider } from '@geist-ui/core'
 import { ExtendedPage } from 'models/app.model'
 import { AppProps } from 'next/dist/shared/lib/router/router'
+import Head from 'next/head'
 import { GlobalStyles } from 'styles/global-styles'
 
 type MyAppProps<PassedProps> = AppProps & {
@@ -16,10 +18,14 @@ const MyApp: FC<MyAppProps<Record<string, never>>> = ({
 
   return getLayout(
     <>
+      <Head>
+        <title>{Component?.pageName ?? 'Book sharing app'}</title>
+      </Head>
       <GeistProvider>
         <CssBaseline />
         <Component {...pageProps} />
       </GeistProvider>
+      <Toaster />
 
       <GlobalStyles />
     </>,
